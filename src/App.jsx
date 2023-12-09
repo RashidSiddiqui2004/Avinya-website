b24a94b8fb1cb040a31b6b5f23f827838333db83
Rashid <siddiqui20042007@gmail.com>
2023-12-09 20:25:58 +0530

    updated sponsors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
-import './App.css' 
+import './App.css'
+import Footer from './components/footerAlternative/Footer'
 import Gallery from './components/gallery/Gallery'
 import RegistrationPoster from './components/register/RegistrationPoster'
 import Sponsors from './components/Sponsors/Sponsors'
@@ -9,26 +10,27 @@ function App() {
   return (
     <>
       <h1 className='text-6xl font-bold font-fairdisplay italic 
-      bg-gradient-to-r from-purple-400 via-green-300 to-blue-500 text-transparent bg-clip-text'>AVINYA
+      bg-gradient-to-r from-purple-400 via-green-300 to-blue-500 text-transparent
+       bg-clip-text'>AVINYA
         <span className='text-red-500 mx-6 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text'>2023</span></h1>
 
-      <div className='flex flex-row mx-[10%] mt-7'>
+      <div className='flex flex-row px-[20%] mt-7'>
         <img src="public/programmer.png" alt="image" className='w-[50%]' />
 
-        <img src="public/VR.png" alt="image" className='w-[50%]'/>
+        <img src="public/VR.png" alt="image" className='w-[50%]' />
       </div>
- 
-      <RegistrationPoster/>
-      <Gallery /> 
+
+      <RegistrationPoster />
+      <Gallery />
 
       <Sponsors />
 
-      <div className='flex flex-row mx-[10%] mt-7'>
-        <img src="public/desk.png" alt="image" className='w-[80%] mx-[15%]' /> 
+      <div className='flex flex-row px-[10%] mt-7'>
+        <img src="public/desk.png" alt="image" className='w-[80%] mx-[15%]' />
       </div>
 
       <div className='my-8'>
-        <h1>Footer here</h1>
+        <Footer />
       </div>
     </>
   )
