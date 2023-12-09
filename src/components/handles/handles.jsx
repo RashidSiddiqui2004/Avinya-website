2f0323078abd2df989c0c09f43438b84311b3965
0-Yashh-0 <yash-ug22@nsut.ac.in>
2023-12-09 13:47:34 +0530

    style

--- src/components/handles/handles.jsx
+++ src/components/handles/handles.jsx
@@ -4,7 +4,7 @@ import styles from './handles.module.css';
 const joinButton = () => {
   return (
     <div className={styles.container}>
-    <h1>JOIN OUR DISCORD SERVER TO STAY UPDATED</h1>
+    <h1 className={styles.heading}>JOIN OUR DISCORD SERVER TO STAY UPDATED</h1>
     <button className={styles.button} role="button">
       JOIN
     </button>
