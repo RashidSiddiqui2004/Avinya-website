f9e801615c302c48a5baf33faffbb2ae029abb51
Jagjeet Singh <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-12-10 16:21:28 +0530

    Create data.jsx

---
+++ src/components/events/data.jsx
@@ -0,0 +1,142 @@
+const dayValues = {
+  1: {
+    block1:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    date: "18 Jan 2024",
+    infoBlock1:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+  },
+  2: {
+    block1:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    date: "19 Jan 2024",
+    infoBlock1:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+  },
+  3: {
+    block1:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    date: "20 Jan 2024",
+    infoBlock1:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+  },
+  4: {
+    block1: "day4 coding competition ",
+    block2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    block8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    date: "21 Jan 2024",
+    infoBlock1: "day4 coding competition ",
+    infoBlock2:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock3:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock4:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock5:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock6:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock7:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+    infoBlock8:
+      "6Compete with creative project ideas and get inspiration for your next project. ",
+  },
+};
+
+export default dayValues;
