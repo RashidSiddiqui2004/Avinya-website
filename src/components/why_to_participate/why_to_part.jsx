444fb0f773f73fbbb5119592644555f95520865e
Chirag Singhal <99918088+CS251000@users.noreply.github.com>
2023-12-10 09:07:06 +0530

    Created why_to_part.jsx

---
+++ src/components/why_to_participate/why_to_part.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './why_to_part.css';
+
+function whypart() {
+    return (
+      <div className='containerwp'>
+      
+        <p className='headingwp'> WHY TO PARTICIPATE IN TECHX 2023</p>
+      
+      <div className='reasonsboxwp'>
+        <div className="reasonwp reason1wp">
+          <h3>GET INSPIRED</h3>
+          <p>Compete with creative project ideas and get inspiration for your next project.</p>
+        </div>
+        <div className="reasonwp reason2wp">
+          <h3>MEET NEW FACES</h3>
+          <p>Meet innovative new peers, make friends and connections.</p>
+        </div>
+        <div className="reasonwp reason1wp">
+          <h3>TECH INSIGHTS</h3>
+          <p>Learn about different tech stacks, see it in action and enhance your skills.</p>
+        </div>
+        <div className="reasonwp reason2wp ">
+          <h3>PRIZE POOL</h3>
+          <p>Prize Pool of worth INR 8,00,000.</p>
+        </div>
+        <div className="reasonwp reason1wp">
+          <h3>OFFLINE EVENT</h3>
+          <p>Get a real world exposure of a Tech Fest.</p>
+        </div>
+        <div className="reasonwp reason2wp">
+          <h3>FREE SWAGS</h3>
+          <p>Get cool goodies and swags to show off to your friends.</p>
+        </div>
+  
+  
+      </div>
+      </div>
+    )
+  }
+
+  export default whypart;
