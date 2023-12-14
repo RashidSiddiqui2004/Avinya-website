3fa695e91d06285c2d907ec11a03398400ae1c56
0-Yashh-0 <yash-ug22@nsut.ac.in>
2023-12-14 14:59:38 +0530

    modify handles

--- src/components/handles/handles.jsx
+++ src/components/handles/handles.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import styles from './handles.module.css';
+import React from "react";
+import styles from "./handles.module.css";
 
-const joinButton = ({CTA}) => {
+const joinButton = ({ CTA }) => {
   return (
     <div className={styles.container}>
-    <h1 className={styles.heading}>JOIN OUR DISCORD SERVER TO STAY UPDATED</h1>
-    <button className={styles.button} role="button">
-      {CTA}
-    </button>
+      <h1 className={styles.heading}>
+        JOIN OUR DISCORD SERVER TO STAY UPDATED
+      </h1>
+      <div className="pointer" target="_blank" style={{ margin: "auto" }}>
+        <a
+          href="https://discord.gg/hhHtDXCBpn"
+          target="_blank"
+          rel="noreferrer"
+          cl
+        >
+          <button className={styles.button}>
+            <p className={styles.p}>JOIN</p>
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
