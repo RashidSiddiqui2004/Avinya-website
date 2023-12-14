605cc70542386ae4574812eed55967774fec52de
oi-taa <ishita21953@gmail.com>
2023-12-14 20:52:19 +0530

    Updated Intro section

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -7,10 +7,7 @@ function IntroFunc() {
       <div className={styles.overlap_group}>
         <div className={styles.rectangle_1} />
         <div className={styles.avinya}>
-          Avinya
-        </div>
-        <div className={styles.avinya_year}>
-          2024
+          Avinya<br/>2024
         </div>
         <p className={styles.intro_text}>
           Yesterday’s body count lottery rounded out to a solid ‘n’ sturdy thirty! Yesterday’s body count lottery
