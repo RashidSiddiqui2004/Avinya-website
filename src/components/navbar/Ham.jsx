112b6dd5e54a21dbf5abed88d123d4c9cbb9f629
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2023-12-22 00:49:15 +0530

    navbar fix for mobile

--- src/components/navbar/Ham.jsx
+++ src/components/navbar/Ham.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Ham.css'; // You can create a separate CSS file for styling
+import './Ham.css'; 
 import { Link } from 'react-router-dom';
 
 const HamburgerMenu = () => {
