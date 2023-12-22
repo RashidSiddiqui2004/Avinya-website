c8ad38d553de6ccb483db83633de9be7bd133b18
Jagjeet Singh <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-12-22 20:31:55 +0530

    Update ImageOverlay.jsx

--- src/components/team/ImageOverlay.jsx
+++ src/components/team/ImageOverlay.jsx
@@ -1,8 +1,10 @@
-import img from "./Left.png";
+// import img from "./Left.png";
 
 const ImageOverlay = () => {
   return (
-    <div className="mt-2 team-members-container flex flex-col justify-center items-center transform hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out">      <div className="team-member relative flex justify-center p-0 ">
+    <div className="mt-5 team-members-container flex flex-col justify-center items-center transform hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out max-w-[475px]">
+      {" "}
+      <div className="team-member flex justify-center p-0 ">
         {/* First Image (SexyMonke.jpg) */}
         <img
           src="/SexyMonke.jpg"
@@ -18,11 +20,9 @@ const ImageOverlay = () => {
 
         {/* Text Overlay */}
         <div className="bg-cyan-400 flex flex-col justify-end w-[80px] py-2 px-2 z-20 ">
-          <div className="font-bold text-4xl text-black transform -rotate-90 -translate-x-5 font-['Drummer']">
+          <div className="font-bold text-3xl text-black transform -rotate-90 -translate-x-5 font-['Drummer']">
             SEXYMONKE
-            <p className="text-black text-4xl font-['LLPixel'] font-normal">
-              Mascot
-            </p>
+            <p className="text-black font-['LLPixel'] font-normal">Mascot</p>
           </div>
         </div>
       </div>
