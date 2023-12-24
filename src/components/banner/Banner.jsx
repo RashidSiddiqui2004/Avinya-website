bfd9c60939cb44064c8ba5e48de63a31ca492c9d
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2023-12-24 17:39:42 +0530

    Banner updates

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import myImage from './avinya_banner.png';
+import myImage from './AVINYA_BANNER1.png';
 import './banner.css';
 
 const MyImageComponent = () => {
   return (
-    <div>
-      <img src={myImage} alt="banner" className='banner' />
+    <div className='banner-container'>
+      <img src={myImage} alt="banner" className='banner-image' />
+      <div className='text-overlay'>
+        <div className='centered' id='avinya'>AVINYA</div>
+        <div className='centered' id='age'>18 - 21</div>
+        <div className='centered' id='month'>January</div>
+      </div>
     </div>
   );
 };
-
 
 export default MyImageComponent;
