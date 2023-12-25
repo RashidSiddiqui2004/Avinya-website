5da77d8460ab5ab62ab7ffb53dc273e29c4c0973
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2023-12-25 23:58:42 +0530

    nav fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,10 +12,10 @@ export default function Navbar({homeUrl}) {
 
       </div>
       <nav className='nav'>
-      <div className='nav-item'><a className='nav-a' href={homeUrl}>HOME</a></div>
-      <div className='nav-item'><a className='nav-a' href="/#about">ABOUT</a></div>
-      <div className='nav-item'><a className='nav-a' href="/#events">EVENTS</a></div>
-      <div className='nav-item'><a className='nav-a' href="/#gallery">GALLERY</a></div>
+      <div className='nav-item home'><a className='nav-a' href={homeUrl}>HOME</a></div>
+      <div className='nav-item about'><a className='nav-a' href="/#about">ABOUT</a></div>
+      <div className='nav-item gall'><a className='nav-a' href="/#events">EVENTS</a></div>
+      <div className='nav-item gall2 gall'><a className='nav-a' href="/#gallery">GALLERY</a></div>
       <div className='nav-item team sponsors'><a className='nav-a team' href="/#sponsors">SPONSORS</a></div>
       <Link to={'/team'}> 
           <div className='nav-item team'><a className='nav-a team' href="/team">OUR TEAM</a></div>
