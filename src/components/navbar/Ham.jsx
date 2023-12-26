74cc1a5133cf23345792ea629b3a3e415dad3365
Jagjeet Singh <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-12-26 18:06:01 +0530

    Update Ham.jsx

--- src/components/navbar/Ham.jsx
+++ src/components/navbar/Ham.jsx
@@ -1,41 +1,57 @@
-import React, { useState } from 'react';
-import './Ham.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Ham.css";
+import { Link } from "react-router-dom";
 
-const HamburgerMenu = ({homeUrl}) => {
+const HamburgerMenu = ({ homeUrl }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-
   return (
-    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
+    <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
       <div className="menu-toggle" onClick={toggleMenu}>
         {isOpen ? (
           <>
-            <div className="cross">X</div>
-            
+            <div className="cross md:-ml-2">X</div>
           </>
         ) : (
           <>
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
+            <div className="px-2 md:-ml-2">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
           </>
         )}
       </div>
       {isOpen && (
-        <ul className="menu-items" onClick={toggleMenu}>
-          <li><a href={homeUrl}>HOME</a></li>
-          <li><a href="/#about">ABOUT</a></li>
-          <li><a href="/#events">EVENTS</a></li>
-          <li><a href="/#gallery">GALLERY</a></li>
-          <li><a href="/#sponsors">SPONSORS</a></li>
-          <li><Link to={'/team'}>
-            <a className='nav-a' href="/team">OUR TEAM</a>
-          </Link>
+        <ul
+          className="menu-items text-left -ml-1 -mt-4 max-w-[60px]"
+          onClick={toggleMenu}
+        >
+          <li>
+            <a href={homeUrl}>HOME</a>
+          </li>
+          <li>
+            <a href="/#about">ABOUT</a>
+          </li>
+          <li>
+            <a href="/#events">EVENTS</a>
+          </li>
+          <li>
+            <a href="/#gallery">GALLERY</a>
+          </li>
+          <li>
+            <a href="/#sponsors">SPONSORS</a>
+          </li>
+          <li>
+            <Link to={"/team"}>
+              <a className="nav-a" href="/team">
+                OUR TEAM
+              </a>
+            </Link>
           </li>
         </ul>
       )}
@@ -43,8 +59,4 @@ const HamburgerMenu = ({homeUrl}) => {
   );
 };
 
-
-
-
 export default HamburgerMenu;
-
