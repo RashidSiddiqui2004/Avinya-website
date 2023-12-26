3a5c9d8bc5437835b58680c946773163bb449c39
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2023-12-27 00:41:23 +0530

    nav fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,15 +8,15 @@ export default function Navbar({ homeUrl }) {
   return (
     <>
       <div className="ham">
-        <Ham homeUrl={"/"} />
+        <Ham homeUrl={"/#"} />
       </div>
       <div class="navbar">
-  <div class="nav-item"><a href={homeUrl}><span>HOME</span></a></div>
-  <div class="nav-item"><a href="/#about"><span>ABOUT</span></a></div>
-  <div class="nav-item"><a href="/#events"><span>EVENTS</span></a></div>
-  <div class="nav-item"><a href="/#gallery"><span>GALLERY</span></a></div>
-  <div class="nav-item"><a href="/#sponsors"><span>SPONSORS</span></a></div>
-  <div class="nav-item"><Link to={"/team"}><span>OUR TEAM</span></Link></div>
+        <div class="nav-item"><a href={homeUrl}><span> HOME</span></a></div>
+        <div class="nav-item"><a href="/#about"><span>ABOUT</span></a></div>
+        <div class="nav-item"><a href="/#events"><span>EVENTS</span></a></div>
+        <div class="nav-item"><a href="/#gallery"><span>GALLERY</span></a></div>
+        <div class="nav-item"><a href="/#sponsors"><span>SPONSORS</span></a></div>
+        <div class="nav-item"><Link to={"/team"}><span>OUR TEAM</span></Link></div>
   </div>
     </>
   );
