54edf2d3e5b2ee0afff9378fe49c398270c25e85
Jagjeet Singh <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-12-26 18:07:38 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import Ham from './Ham' 
-import './Navbar.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import Ham from "./Ham";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
-
-export default function Navbar({homeUrl}) {
+export default function Navbar({ homeUrl }) {
   return (
     <>
-      <div className='ham'>
-        <Ham homeUrl={'/'}/>
-
+      <div className="ham">
+        <Ham homeUrl={"/"} />
       </div>
-      <nav className='nav'>
-      <div className='nav-item home'><a className='nav-a' href={homeUrl}>HOME</a></div>
-      <div className='nav-item about'><a className='nav-a' href="/#about">ABOUT</a></div>
-      <div className='nav-item gall'><a className='nav-a' href="/#events">EVENTS</a></div>
-      <div className='nav-item gall2 gall'><a className='nav-a' href="/#gallery">GALLERY</a></div>
-      <div className='nav-item team sponsors'><a className='nav-a team' href="/#sponsors">SPONSORS</a></div>
-      <Link to={'/team'}> 
-          <div className='nav-item team'><a className='nav-a team' href="/team">OUR TEAM</a></div>
-        </Link>
-        
-        
-        
-        
-        
+      <nav className="nav mt-12">
+        <div className="nav-item home">
+          <a className="nav-a" href={homeUrl}>
+            HOME
+          </a>
+        </div>
+        <div className="nav-item about">
+          <a className="nav-a" href="/#about">
+            ABOUT
+          </a>
+        </div>
+        <div className="nav-item gall">
+          <a className="nav-a" href="/#events">
+            EVENTS
+          </a>
+        </div>
+        <div className="nav-item gall2 gall">
+          <a className="nav-a" href="/#gallery">
+            GALLERY
+          </a>
+        </div>
+        <div className="nav-item team sponsors">
+          <a className="nav-a team" href="/#sponsors">
+            SPONSORS
+          </a>
+        </div>
+        <div className="nav-item team">
+          <Link to={"/team"} className="nav-a team">
+            OUR TEAM
+          </Link>
+        </div>
       </nav>
-      
     </>
-  )
+  );
 }
-
