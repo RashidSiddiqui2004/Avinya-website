df61341f39a6730b526a1202324ef7c755a9f56a
pratyushcode <pratyush.sinha.ug21@nsut.ac.in>
2024-01-06 22:21:24 +0530

    updated the event

--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -6,47 +6,69 @@ import styling from "./event.module.css";
 import styling2 from "./modal.module.css";
 import eventContent from "./eventdesc";
 
-
 const Mdl = (props) => {
-
   const [form, setForm] = useState(false);
 
   return (
     props.mdlstate && (
       <div className="fixed top-0 left-0 w-[100vw] h-full flex items-center justify-center z-50">
-        {form ?
+        {form ? (
           <div className={styling2.parent_div}>
+            <h1 className={styling2.head2}>
+              {eventContent[props.day - 1][props.event_id - 1].title}{" "}
+            </h1>
 
-            <h1 className={styling2.head2}>{eventContent[props.day - 1][props.event_id - 1].title} </h1>
+            <button className={styling2.btn3} onClick={() => setForm(false)}>
+              X
+            </button>
 
-            <button className={styling2.btn3} onClick={() => setForm(false)}>X</button>
-
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc9CEOW_LkEuon4mwjrnvyoXj-CxXpn25GHKxcIeIh4JB80Zg/viewform?embedded=true"
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSc9CEOW_LkEuon4mwjrnvyoXj-CxXpn25GHKxcIeIh4JB80Zg/viewform?embedded=true"
               className="w-full h-[440px] overflow-y-hidden overflow-hidden"
-              frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+            >
+              Loading…
+            </iframe>
           </div>
-          :
+        ) : (
           <div className={styling2.parent_div}>
-
             <div className={styling2.gradient_div}>
-              <h1 className={styling2.head}>{eventContent[props.day - 1][props.event_id - 1].title} </h1>
+              <h1 className={styling2.head}>
+                {eventContent[props.day - 1][props.event_id - 1].title}{" "}
+              </h1>
               <p className={styling2.info}>
                 {eventContent[props.day - 1][props.event_id - 1].event_info}
-                <div style={{ width: "fit-content", margin: "2% 1% 1% 1%" }}> Time : {eventContent[props.day - 1][props.event_id - 1].time_span}
-                </div><div style={{ width: "fit-content", margin: "1%" }}> Date : {eventContent[props.day - 1][props.event_id - 1].date} </div>
-                <div style={{ width: "fit-content", margin: "1%" }}>No of participants registered : {eventContent[props.day - 1][props.event_id - 1].no_of_participants} </div>
+                <div style={{ width: "fit-content", margin: "2% 1% 1% 1%" }}>
+                  {" "}
+                  Time :{" "}
+                  {eventContent[props.day - 1][props.event_id - 1].time_span}
+                </div>
+                <div style={{ width: "fit-content", margin: "1%" }}>
+                  {" "}
+                  Date : {
+                    eventContent[props.day - 1][props.event_id - 1].date
+                  }{" "}
+                </div>
+                <div style={{ width: "fit-content", margin: "1%" }}>
+                  No of participants registered :{" "}
+                  {
+                    eventContent[props.day - 1][props.event_id - 1]
+                      .no_of_participants
+                  }{" "}
+                </div>
               </p>
               <button className={styling2.btn2} onClick={() => setForm(true)}>
                 Registration opening soon.....!
               </button>
             </div>
-            <button className={styling2.btn1} onClick={props.close}>X</button>
+            <button className={styling2.btn1} onClick={props.close}>
+              X
+            </button>
           </div>
-        }
-
+        )}
       </div>
-
     )
   );
 };
@@ -55,7 +77,7 @@ function DayDataLeft(props) {
   const [mdl, setmdl] = useState(false);
   const toggle_mdl = () => {
     setmdl(!mdl);
-  }
+  };
   return (
     <div className={styling.master1}>
       <div className={styling.smallBox}>
@@ -63,11 +85,15 @@ function DayDataLeft(props) {
         <h1 className={styling.num}>{props.num}</h1>
       </div>
       <div className={styling.bigBox}>
-        <Mdl mdlstate={mdl} close={toggle_mdl} event_id={Number(props.num)} day={props.day} />
+        <Mdl
+          mdlstate={mdl}
+          close={toggle_mdl}
+          event_id={Number(props.num)}
+          day={props.day}
+        />
         <p className={styling.content}>{props.context}</p>
         <button
           className={`${styling["infoLinkLeft"]} text-xs lg:text-base rounded-none focus:outline-none`}
-
           onClick={toggle_mdl}
         >
           More Info
@@ -77,15 +103,56 @@ function DayDataLeft(props) {
   );
 }
 
+function DayDatamiddle(props) {
+  const [mdl, setmdl] = useState(false);
+  const toggle_mdl = () => {
+    setmdl(!mdl);
+  };
+  return (
+    <div className={styling.master1}>
+      <div className={styling.smallBox}>
+        <div className={styling.triangleLeft}></div>
+        <h1 className={styling.num}>{props.num}</h1>
+      </div>
+      <div className={styling.bigBox2}>
+        <Mdl
+          mdlstate={mdl}
+          close={toggle_mdl}
+          event_id={Number(props.num)}
+          day={props.day}
+        />
+        <p className={styling.content}>{props.context}</p>
+       
+          <button
+            className={`${styling["infoLinkLeft2"]} text-xs sm:text-small lg:text-base rounded-none focus:outline-none`}
+            onClick={toggle_mdl}
+          >
+            More Info
+          </button>
+        
+      </div>
+      <div className={styling.smallBox}>
+        <div className={styling.triangleRight}></div>
+        <h1 className={styling.num}>{props.num}</h1>
+      </div>
+    </div>
+  );
+}
+
 function DayDataRight(props) {
   const [mdl, setmdl] = useState(false);
   const toggle_mdl = () => {
     setmdl(!mdl);
-  }
+  };
   return (
     <div className={styling.master2}>
       <div className={styling.bigBox}>
-        <Mdl mdlstate={mdl} close={toggle_mdl} event_id={Number(props.num)} day={props.day} />
+        <Mdl
+          mdlstate={mdl}
+          close={toggle_mdl}
+          event_id={Number(props.num)}
+          day={props.day}
+        />
         <p className={styling.content}>{props.context}</p>
         <button
           className={`${styling["infoLinkRight"]} text-xs lg:text-base rounded-none focus:outline-none`}
@@ -113,18 +180,25 @@ DayDataRight.propTypes = {
   context: ReactPropTypes.string,
   info: ReactPropTypes.string,
 };
+DayDatamiddle.propTypes = {
+  num: ReactPropTypes.string,
+  context: ReactPropTypes.string,
+  info: ReactPropTypes.string,
+};
 
 function Event() {
   const [activeDay, setActiveDay] = useState(1);
 
-  const numberofEvents = [3, 4, 5, 4];
+  const numberofEvents = [3, 3, 3, 1];
 
   const handleChange = (event, newValue) => {
     setActiveDay(newValue);
   };
 
-  const divArray = Array.from({ length: numberofEvents[activeDay - 1]/2}, (_, index) => index + 1);
-
+  const divArray = Array.from(
+    { length: numberofEvents[activeDay - 1] / 2 },
+    (_, index) => index + 1
+  );
 
   return (
     <div className={`${styling["main"]}`} id="events">
@@ -133,7 +207,7 @@ function Event() {
       >
         EVENTS
       </h1>
-      <div className="flex flex-col text-center items-center justify-center mb-5">
+      <div className="flex flex-col  text-center items-center justify-center mb-5">
         <Paper square>
           <Tabs
             value={activeDay}
@@ -170,11 +244,9 @@ function Event() {
       </h1>
 
       <div>
-
         {divArray.map((value, index) => {
-
           return (
-            <div key={index + 1} className={styling.master}>
+            <div className={`text-2xl ${styling.master}`} key={index + 1}>
               <>
                 <DayDataLeft
                   num={(index + 1) * 2 - 1}
@@ -190,31 +262,37 @@ function Event() {
                   day={activeDay}
                 />
               </>
-
-
             </div>
-          )
+          );
         })}
-
-        {
-          numberofEvents[activeDay - 1] % 2 != 0 ?
-            <>
-              <DayDataLeft
-                num={numberofEvents[activeDay - 1]}
-                context={dayValues[activeDay].headings[numberofEvents[activeDay - 1]-1]}
-                // info={dayValues[activeDay].infoBlock1}
-                day={activeDay}
-              />
-            </>
-            :
+        <div>
+          {numberofEvents[activeDay - 1] % 2 !== 0 ? (
+            <div className="master4">
+              <>
+                <DayDatamiddle
+                  num={numberofEvents[activeDay - 1]}
+                  context={
+                    dayValues[activeDay].headings[
+                      numberofEvents[activeDay - 1] - 1
+                    ]
+                  }
+                  day={activeDay}
+                />
+                {/* <div className={styling.smallBox}>
+                  <div className={styling.triangleRight}></div>
+                  <h1 className={styling.num}>
+                    {numberofEvents[activeDay - 1]}
+                  </h1>
+                </div> */}
+              </>
+            </div>
+          ) : (
             ""
-        }
-
+          )}
+        </div>
       </div>
 
-
       <div className={styling.allContent}>
-
         {/* <div className={styling.master}>
           <DayDataLeft
             num="1"
@@ -273,7 +351,6 @@ function Event() {
             day={activeDay}
           />
         </div> */}
-
       </div>
     </div>
   );
